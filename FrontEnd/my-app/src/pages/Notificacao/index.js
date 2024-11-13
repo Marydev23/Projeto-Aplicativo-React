1fe4d484df14f3f34b4ef3370fc27828e479e1a8
Juliano Teodoro <julianoteodoro2002@gmail.com>
2024-11-12 21:34:18 -0300

    Retirando notificação

--- FrontEnd/my-app/src/pages/Notificacao/index.js
+++ FrontEnd/my-app/src/pages/Notificacao/index.js
@@ -9,9 +9,6 @@ import {
   Platform 
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
 
 const notificationsData = [
   { id: '1', message: 'Você recebeu uma nova mensagem.' },
@@ -21,115 +18,11 @@ const notificationsData = [
 
 ];
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-    shouldShowAlert: true 
-  }),
-})
-
 export default function Notificacao() {
 
-  async function registerForPushNotificationsAsync() {
-    let token;
-
-    if (Platform.OS === 'android') {
-
-      await Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
-      });
-  
-    }
-
-    if(Device.isDevice){
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
-      // Learn more about projectId:
-      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-      // EAS projectId is used here.
-      try {
-        const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
-        if (!projectId) {
-          throw new Error('Project ID not found');
-        }
-        token = (
-          await Notifications.getExpoPushTokenAsync({
-            projectId,
-          })
-        ).data;
-        console.log(token);
-      } catch (e) {
-        token = `${e}`;
-      }
-      
-    }
-    else {
-      alert('Must use physical device for Push Notifications');
-    }
-    
-    setExpoToken(token)
-  
-    return token;
-  }
-  
-  async function schedulePushNotification() {
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: "Chegou!!",
-        body: "Sua entrega chegou!!",
-        data: {}
-      },
-      trigger: {
-        seconds: 5
-      }
-    })
-      
-  }
-
-  const [expoToken, setExpoToken] = useState("")
-  const [channels, setChannels] = useState([]);
-
-  const notificationReceiverRef = useRef()
-  const notificationResponseRef = useRef()
-
   useEffect(() =>  {
-    registerForPushNotificationsAsync().then(token => token && setExpoToken(token));
 
-    if (Platform.OS === 'android') {
-       Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []));
-    }
-
-    notificationReceiverRef.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    notificationReceiverRef.current = Notifications.addNotificationReceivedListener(notifications => {
-      console.log("Notification received: " + notifications)
-    })
-
-    notificationResponseRef.current = Notifications.addNotificationResponseReceivedListener(notification => {
-      console.log("Notification response received: " + notification)
-
-    })
-
-   schedulePushNotification();
     return () => {
-      notificationReceiverRef.current &&
-        Notifications.removeNotificationSubscription(notificationReceiverRef.current);
-      notificationResponseRef.current &&
-        Notifications.removeNotificationSubscription(notificationResponseRef.current);
     };
   }, []);
 
@@ -150,7 +43,6 @@ export default function Notificacao() {
       <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
         <Text style={styles.backButtonText}>Voltar</Text>
       </TouchableOpacity>
-      <Text>Your expo push token: {expoToken}</Text>
 
     </View>
   );
